88429f4a3ef3a26c9f6affe3e430240768de3009
Joshua McCue <joshuamccue@Joshuas-MBP.fios-router.home>
2020-03-25 16:52:35 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import QuoteBox from './Container/QuoteBox/QuoteBox';
+
+import './App.css';
+
+class App extends Component {
+ render () {
+  return (
+    <div className="App">
+      <QuoteBox />
+    </div>
+  );
+ }
+}
+
+export default App;
